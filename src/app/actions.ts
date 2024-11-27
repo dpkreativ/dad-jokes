a35738d10a1bca62ad31c0adec4bf05b8fc56d1a
Divine Orji <dpkreativ@gmail.com>
2024-11-27 11:01:01 +0100

    hotfix: tweaking things

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,7 +2,7 @@
 
 import { Joke, ZenrowsApiResponse, RedditPost } from "@/types/jokes";
 
-const apikey = process.env.ZENROWS_API_KEY;
+const apikey = process.env.ZENROWS_API_KEY!;
 const subredditBaseUrl = "https://www.reddit.com/r/dadjokes/";
 const maxJokes = 50;
 
@@ -17,6 +17,10 @@ export const fetchJokes = async (): Promise<Joke[]> => {
       const response = await fetch(
         `https://api.zenrows.com/v1/?url=${url}&apikey=${apikey}&premium_proxy=true`
       );
+      if (!response.ok) {
+        throw new Error(`Failed to fetch jokes: ${response.statusText}`);
+      }
+
       const data: ZenrowsApiResponse = await response.json();
 
       const posts = data.data.children;
